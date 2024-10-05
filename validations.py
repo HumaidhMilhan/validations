afffa1b73310821f83dbeaec57830446a4eb4b12
Humaidh.M <humaidhmilhan456@gmail.com>
2024-10-05 19:30:36 +0530

    Additional documentation for added context

--- validations.py
+++ validations.py
@@ -7,9 +7,10 @@ def validate_user(username, minlen):
     raise TypeError("username must be a string")
   if minlen < 1:
     raise ValueError("minlen must be at least 1")
-    
+  # Usernames should have more characters than minlen defined
   if len(username) < minlen:
       return False
+  # Usernames should only contain Alphanumeric characters  
   if not username.isalnum():
       return False
   # Usernames can't begin with a number
